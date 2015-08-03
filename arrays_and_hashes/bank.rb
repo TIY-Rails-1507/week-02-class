fba8c1655f425783f800ec1a5131aca0528e2e39
Daryn Holmes <daryn@Mac.local>
2015-08-03 20:36:28 +0100

    Bank account with a hash for accounts

--- arrays_and_hashes/bank.rb
+++ arrays_and_hashes/bank.rb
@@ -1,18 +1,20 @@
 class Bank
 
 	def initialize
-		@accounts = []
+		@accounts = {}
 	end
 
-	def transfer(from_account, to_account, amount)
-		from_account = @accounts.find {|account| account.number == from_account }
-		to_account = @accounts.find {|account| account.number == to_account }
+	def transfer(from_account_number, to_account_number, amount)
+		# from_account = @accounts.find {|account| account.number == from_account }
+		# to_account = @accounts.find {|account| account.number == to_account }
+		from_account = @accounts[from_account_number]
+		to_account = @accounts[to_account_number]
 		from_account.withdraw(amount)
     	to_account.deposit(amount)	
 	end
 
 	def add_account(account)
-		@accounts << account
+		@accounts[account.number] = account
 	end
 
 
