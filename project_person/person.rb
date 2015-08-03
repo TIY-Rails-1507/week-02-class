172da9d9cc5f5eb385a6482c2209dd88628407e9
Daryn Holmes <daryn@Mac.local>
2015-08-03 19:08:58 +0100

    Added attribute accessor to the person class

--- project_person/person.rb
+++ project_person/person.rb
@@ -1,11 +1,22 @@
 class Person
 
+	# attr_reader :first_name
+	# attr_writer :first_name
+
+	attr_accessor :first_name
+
+	# def first_name()
+	# 	@first_name
+	# end
+
+	# def first_name=(name)
+	# 	@first_name = name
+	# end
+
 	def initialize(first_name, last_name)
 		@first_name = first_name
 		@last_name = last_name
 	end
-
-
 
 	def full_name()
 		"#{@first_name} #{@last_name}"
--- project_person/person_test.rb
+++ project_person/person_test.rb
@@ -12,5 +12,13 @@ class TestPerson < MiniTest::Unit::TestCase
     full_name = person.full_name()
     assert_equal('John Snow', full_name)
   end
+
+  def test_setting_first_name
+    first_name = 'John'
+    last_name = 'Snow'
+    person = Person.new(first_name, last_name)
+    person.first_name = "Sue"
+    assert_equal('Sue', person.first_name)
+  end
   
 end
