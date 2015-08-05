11edd95eebf5ea37e414da840416a941be64f89e
meraymond2 <raymond.michael@gmail.com>
2015-08-05 17:31:45 +0100

    Fixed unintended commit, files should be back to before

--- homework/account.rb
+++ homework/account.rb
@@ -1,20 +1,15 @@
 class Account
 
-attr_reader :balance
-
 	def initialize(opening_balance)
 		@balance = opening_balance
 	end
 
-	# def balance()
-	# 	@balance # in ruby the last line is always returned
-	# end
+	def balance()
+		@balance # in ruby the last line is always returned
+	end
 
 	def withdraw(amount)
-		if amount > @balance
-			raise RuntimeError, 'Negative balance error.'
-		else @balance -= amount
-		end
+		@balance = @balance - amount
 	end
 
 	def deposit(amount)
--- homework/main_interactive.rb
+++ homework/main_interactive.rb
@@ -6,33 +6,3 @@
 # if they try to go over their balance, the account class will throw an exception
 # rescue the exception and show an error message
 # then ask if they want to exit or withdraw again (i.e go back to line 2)
-
-require_relative 'account'
-acc1 = Account.new(100)
-msg1 = 'Would you like to withdraw funds or exit? '
-msg2 = 'How much would you like to withdraw? '
-err_msg = "You may not withdraw more than your balance."
-# msg3 = "Your new balance is: #{acc1.balance}"
-# doesn't work, since it only displays 100
-
-loop do 
-	print msg1
-	ans = gets.strip.downcase
-	if ans == "exit"
-		break
-	elsif ans == 'withdraw'
-			begin
-				print msg2
-				amount = gets.strip.to_i
-				acc1.withdraw(amount) 
-			rescue 
-				puts err_msg
-			else
-				puts "Your current balance is: #{acc1.balance}."
-			end
-	else puts "Invalid Answer"
-	end
-end
-
-
-
